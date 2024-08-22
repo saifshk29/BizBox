cb07fa494e83c32a954a4d156f841b3b1f2d7e6d
Saif Shaikh <87814660+saifshk29@users.noreply.github.com>
2024-08-22 22:09:47 +0530

    Update api

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     headers: {
         accept: "application/json",
-        Authorization: '',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MGI0ZTYwYzQwOWM3NTMxZDUxMGE2NDViOWQxMjE0YSIsIm5iZiI6MTcyMzA1MDA1NS41MzA3MDMsInN1YiI6IjY2YjNhNmZkNjA2YTdlMjdmM2UxODNlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5cvY9EsJTwugR0iAQUiZNB_i7mjnCyZCS2bsaD4wipI',
     },
 });
 
